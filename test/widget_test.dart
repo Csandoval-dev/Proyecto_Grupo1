2ba6c17be9bd9431d7623094856a6cd1e50707eb
Csandoval-dev <156733491+Csandoval-dev@users.noreply.github.com>
2025-05-12 19:25:58 -0600

    Refactor MyApp class to extend StatelessWidget and improve widget structure

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -29,6 +29,24 @@ void main() {
   });
 }
 
-class MyApp {
-  const MyApp();
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Counter App'),
+        ),
+        body: const Center(
+          child: Text('0'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.add),
+        ),
+      ),
+    );
+  }
 }
